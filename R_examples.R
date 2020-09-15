076d68c0f8b1d69105f5748e7fb7b30a4b0ff3f6
Kiley Graim <kgraim@gmail.com>
2020-09-15 14:52:08 -0400

    Update R_examples.R

--- R_examples.R
+++ R_examples.R
@@ -15,6 +15,9 @@ ggplot(dat, aes(X, Y, fill= Z)) + geom_tile() +
 ggplot(dat, aes(X, Y, fill= Z)) + geom_tile() + 
   scale_fill_gradientn(colors=c("grey90","white","maroon"))
 
+## Increase all font sizes in the plot
+ggplot(dat, aes(X, Y, fill= Z)) + geom_tile() + theme(text = element_text(size = 20))
+
 
 ## Remove A-Z from all strings (A-Z or some pattern to replace)
 dat.names <- paste0(sample(1:20, 10), sample(LETTERS, 10)) # sample output:  [1] "7M"  "16P" "12J" "8V"  "14L" "19D" "15E" "6Z"  "18B" "13Q"
